63e186e91c641a73e69976516132a37bcbae4bcb
vishal choudhary <vishal129.vk@gmail.com>
2018-10-31 10:29:36 +0530

    Add navbar and edit some program

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 import UiCard from './components/card/UiCard';
+import NavBar from './components/navBar/navBar';
+// import ProfileCard from './components/profileCard/profileCard';
+import "./App.css";
 class App extends Component {
   render() {
-    const avatar = "https://images.mapsofindia.com/who_is_who_new/2018/02/02051959/who-is-who-sports-sachin-tendulkar.jpg";
-    const image="https://www.extremetech.com/wp-content/uploads/2016/05/AppleMusicFeature.jpg";
+    const avatar = "https://avatars1.githubusercontent.com/u/14358616?s=400&u=9f6ee034ad601bd6b83f4a60547888de02795418&v=4"
+    const image = "https://www.extremetech.com/wp-content/uploads/2016/05/AppleMusicFeature.jpg";
     return (
       <div>
-        <UiCard
-          nickname="Parth"
-          avatar={avatar}
-          time="50min"
-          username="gamparth"
-          caption="Sachin, is my hero"
-          image={image} />
-        <UiCard
-          nickname="Parth"
-          avatar={avatar}
-          time="50min"
-          username="gamparth"
-          caption="Sachin, is my hero"
-          image={image} />
-      </div>
+        <NavBar></NavBar>
+        <div className="App" ref="Main">
+          <UiCard
+            nickname="Vishi choudhary"
+            avatar={avatar}
+            time="50min"
+            caption="Sachin, is my hero"
+            image={image} />
+          <UiCard
+            nickname="Vishi choudhary"
+            avatar={avatar}
+            time="50min"
+            caption="Sachin, is my hero"
+            image={image} />
+        </div>
+      </div >
     );
   }
 }
--- src/components/card/UiCard.js
+++ src/components/card/UiCard.js
@@ -3,15 +3,27 @@ import React, { Component } from "react";
 import "./Uicard.css";
 
 class UiCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttonLiked: true
+        }
+    }
+    changeColor() {
+        this.setState({
+            buttonLiked: !this.state.buttonLiked
+        })
+    }
     render() {
         const nickname = this.props.nickname;
         const avatar = this.props.avatar;
         const image = this.props.image;
         const caption = this.props.caption;
         const postTime = this.props.time;
+        let likeBtnCls = this.state.buttonLiked ? "like-button" : "liked-button";
 
         return (
-            <article className="Post" ref="Post">
+            <div className="col-6 offset-3 Post" ref="Post">
                 <header>
                     <div className="Post-user">
                         <div className="Post-user-avatar">
@@ -32,13 +44,13 @@ class UiCard extends Component {
                         <img alt={caption} src={image} />
                     </div>
                 </div>
-                <button className="like-button">
-                    <i className="fa fa-heart"></i> Like
+                <button className={likeBtnCls} onClick={this.changeColor.bind(this)}>
+                    <i className="fa fa-heart" ></i> Like
                 </button>
                 <button className="comment-button">
                     <i className="fa fa-comment"></i> Comment
                 </button>
-            </article>
+            </div>
         );
     }
 }
