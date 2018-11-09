60f3af480effe4a98b5db500dd35cc64269f496e
vishal choudhary <vishal129.vk@gmail.com>
2018-11-09 18:27:10 +0530

    Add login container and component

---
+++ src/Containers/Login/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LoginComp from '../../Components/Login/Login';
+
+class Login extends Component {
+    render() {
+        return (
+            <LoginComp/>
+        )
+    }
+}
+
+export default Login;
