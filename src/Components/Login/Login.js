239661b8e0a3966912ab05a0af7c02fd76774973
souravirus <souravsahoo66@yahoo.com>
2018-11-09 20:37:50 +0530

    Added login inputs

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Card, CardBody, CardTitle, CardSubtitle, Row, Col, Button, Form, FormGroup, Label, Input, FormText} from 'reactstrap';
 
 function Login() {
-    return <h1> I am suppose to be Login page</h1>;
+    return (
+        <Form>
+            <FormGroup>
+                <Row>
+                    <Col xs={{size: 5, offset: 4}} sm={{size: 3, offset: 4}}>
+                        <Label for="username">Username</Label>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={{size:5, offset: 4}} sm={{size: 3, offset: 4}}>
+                        <Input id="username" name="username" placeholder="Username"/>
+                    </Col>
+                </Row>
+            </FormGroup>
+            <FormGroup>
+                <Row>
+                    <Col xs={{size: 5, offset: 4}} sm={{size: 3, offset: 4}}>
+                        <Label for="password"> Password </Label>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={{size: 5, offset: 4}} sm={{size: 3, offset: 4}}>
+                        <Input type="password" id ="password" name="password" placeholder="Password"/>
+                    </Col>
+                </Row>
+            </FormGroup>
+            <FormGroup>
+                <Row>
+                    <Col xs={{size: 5, offset: 4}} sm={{size: 3, offset: 4}}>
+                        <Button>Submit</Button>
+                    </Col>
+                </Row>
+            </FormGroup>
+        </Form>
+    )
 }
 
 export default Login;
