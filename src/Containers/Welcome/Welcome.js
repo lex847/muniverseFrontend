c23d8c16f27f30866e60254383016f6715420b88
vishal choudhary <vishal129.vk@gmail.com>
2018-11-09 18:26:42 +0530

    Add welcome screen

---
+++ src/Containers/Welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+    render() {
+        return (
+            <div>
+                I am suppose to be initial screen
+            </div>
+        )
+    }
+}
+
+export default Welcome;
